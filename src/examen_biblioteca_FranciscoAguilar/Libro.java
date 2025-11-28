24e0963a3f54e845f884d8ae6bd3ef51b386be01
francisco-aguilar <aguilar.cifra25@triana.salesianos.edu>
2025-11-28 10:45:34 +0100

    Precio base configurado correctamente

---
+++ src/examen_biblioteca_FranciscoAguilar/Libro.java
@@ -0,0 +1,14 @@
+package examen_biblioteca_FranciscoAguilar;
+
+public class Libro {
+
+	public static void main(String[] args) {
+		
+		String titulo;
+		int precio = 20;
+		
+		System.out.println("Iniciando...");
+
+	}
+
+}
